94f2b5ac6954718ec5ebe929d8a7726979e1b442
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-09-30 16:41:12 -0700

    has potential validation tool

--- tests/application_tests.js
+++ tests/application_tests.js
@@ -9,3 +9,17 @@ QUnit.test("Application#hideHeader", function(assert) {
   assert.equal($('body > h1').is(':visible'), false, 'Header should not be visible.');
   header.remove();
 });
+
+QUnit.test("Application#hideHeader failure", function(assert) {
+  var application = new Application();
+  assert.throws(function() {
+    application.hideHeader();
+  }, /could not hide/i, 'Expected hiding of header to fail with no header present.');
+});
+
+// QUnit.test("expecting number for credit card", function(assert) {
+//   var application = new Application();
+//   var cardNumber = $('inputcredit'.appendTo('form'))
+//   application.validate();
+//   assert.equal($(cardNumber).is('555'), true, 'This is a number.');
+// });
